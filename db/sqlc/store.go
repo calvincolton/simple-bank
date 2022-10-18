457e3d3b21eb451c67d7141a5e6d0252bbedc12f
Calvin Colton <calvincolton@Calvins-MBP-2.hsd1.ca.comcast.net>
2022-10-18 13:00:04 -0700

    attempt removing serializable transactions

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -28,9 +28,7 @@ func NewStore(db *sql.DB) Store {
 
 // ExecTx executes a function within a database transaction
 func (store *SQLStore) execTx(ctx context.Context, fn func(*Queries) error) error {
-	tx, err := store.db.BeginTx(ctx, &sql.TxOptions{
-		Isolation: sql.LevelSerializable,
-	})
+	tx, err := store.db.BeginTx(ctx, nil)
 	if err != nil {
 		return err
 	}
